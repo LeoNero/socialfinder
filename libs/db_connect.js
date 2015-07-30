ac3c035937fce41b029e770b3c3cbe795504558c
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2015-07-29 22:42:07 -0300

    Tentando colocar no ar

--- libs/db_connect.js
+++ libs/db_connect.js
@@ -6,7 +6,7 @@ var env_url = {
 };
 
 module.exports = function() {
-    var env = process.env.NODE_ENV || process.env.MONGOLAB_URI || "development";
+    var env = process.env.NODE_ENV || process.env.MONGOLAB_URI || "mongodb://localhost/socialfinder";
 
     if (!single_connection) {
         single_connection = mongoose.connect(env, function(err, res) {
