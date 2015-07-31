e7247553a95db6b262273c4d0f53b9f0a919fc38
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2015-07-31 20:53:14 -0300

    Por estilo

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
        'clientID'      : '990328931018043',
        'clientSecret'  : '1d76a10a22f2a14f7d4a62fd9930d663',
-       'callbackURL'   : 'http://socialfinder.herokuapp.com/auth/facebook/callback'
+       'callbackURL'   : '/auth/facebook/callback'
    }
 };
